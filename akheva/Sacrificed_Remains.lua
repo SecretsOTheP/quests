21e6375458c28d73e9fc00d3759acf30e6de03b9
Edalyn <espause0703@gmail.com>
2025-12-22 12:10:12 -0500

    Boost shimmering presence to 50%

--- akheva/Sacrificed_Remains.lua
+++ akheva/Sacrificed_Remains.lua
@@ -1,5 +1,5 @@
 function event_death_complete(e)
-	if(math.random(1,100) <= 10) then -- 10% chance
+	if(math.random(1,100) <= 50) then -- 50% chance
 		e.self:Emote("disolves into a shimmering presence, coalescing into a swirl of smoke and energy.");
 		eq.spawn2(179018,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
 	else
