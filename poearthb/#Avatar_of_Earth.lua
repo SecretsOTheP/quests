eede4033ff856f929034aacec583109b93b9e1ba
kyinen <178090649+kyinen@users.noreply.github.com>
2026-09-03 18:22:26 -0500

    Improve Plane of Earth encounter recovery

--- poearthb/#Avatar_of_Earth.lua
+++ poearthb/#Avatar_of_Earth.lua
@@ -1,12 +1,20 @@
 local PLANAR_PROJECTION_TYPE = 222041; -- Essence_of_Earth
+local COUNCIL_SPAWN_IDS = { 369377, 369380, 369382, 369384, 369386, 369387, 369376, 369378, 369379, 369381, 369383, 369385 };
+
+function SetCouncilRespawn(seconds)
+	for _, id in ipairs(COUNCIL_SPAWN_IDS) do
+		eq.update_spawn_timer(id, seconds * 1000);
+	end
+end
 
 function event_death_complete(e)
 	eq.spawn2(PLANAR_PROJECTION_TYPE, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), 0);
 	eq.signal(PLANAR_PROJECTION_TYPE, e.killer:GetID()); -- e.killer for death_complete is somebody with kill rights, not death blow
+	SetCouncilRespawn(237600); -- restart the Council 2 days, 18 hours after Avatar dies
 end
 
 function event_spawn(e)
-	eq.set_timer("depop", 2100000);
+	eq.set_timer("depop", 9000000); -- 2.5 hours; paused while Avatar is engaged
 end
 
 function event_combat(e)
@@ -20,6 +28,7 @@ end
 function event_timer(e)
 	if ( e.timer == "depop" ) then
 		eq.debug("Avatar of Earth depop");
+		SetCouncilRespawn(420); -- failed Avatar attempt: restore the Council after 7 minutes
 		eq.depop();
 	end
 end
